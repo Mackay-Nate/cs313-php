96068bf5b76e4d0863f1f34cbe4aff43b3c1e296
Nate <mac17039@byui.edu>
2019-09-27 09:46:39 -0600

    created a homepage and added css

--- web/index.php
+++ web/index.php
@@ -3,13 +3,13 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nate Mackay's CS 313 assignments</title>
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="week02/week02.css">
   </head>
 
 <body>
   <h1>CS 313 Assignment</h1>
     <ul>
-      <li><a href="index.php">Coming soon</a></li>
+      <li><a href="week02/week02.html">Week 02: Homepage</a></li>
       <li><a href="index.php">Coming soon</a></li>
       <li><a href="index.php">Coming soon</a></li>
       <li><a href="index.php">Coming soon</a></li>
