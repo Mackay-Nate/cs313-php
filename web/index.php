90772d87dd274289b6d74fc9d1490c49c4c0ebe3
Nate <mac17039@byui.edu>
2019-09-27 12:58:34 -0600

    trying to add php

--- web/index.php
+++ web/index.php
@@ -7,6 +7,19 @@
   </head>
 
 <body>
+<?php
+    $header = ""; 
+      $header += "<header>";
+      $header += "<h1>Nate Mackay</h1>";"
+      $header += "<nav>";"
+      $header += "<a href="week02/week02.html">Homepage</a>";"
+      $header += "<a href="index.html">CS 313 Assignments</a>";"
+      $header += "</nav>";"
+      $header += "</header>";" 
+
+      echo $header;
+  ?>
+    
   <h1>CS 313 Assignment</h1>
     <ul>
       <li><a href="week02/week02.html">Week 02: Homepage</a></li>
