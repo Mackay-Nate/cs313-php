82f3559ea1aaf4e8ae504b46ccb6d8428fef4b3a
Nate <mac17039@byui.edu>
2019-09-27 09:27:34 -0600

    moved previous index.php over to info.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,29 @@
+<!DOCYTPE html> 
+<html lang="en-us">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nate Mackay's CS 313 assignments</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+  </head>
+
+<body>
+  <h1>CS 313 Assignment</h1>
+    <ul>
+      <li><a href="index.php">Coming soon</a></li>
+      <li><a href="index.php">Coming soon</a></li>
+      <li><a href="index.php">Coming soon</a></li>
+      <li><a href="index.php">Coming soon</a></li>
+    </ul>
+    
+
+
+
 <?php
 
   phpinfo();
 
 ?>
+
+
+</body>
+</html>
