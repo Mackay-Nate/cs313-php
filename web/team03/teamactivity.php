525e885615e23e3ef54b0f8c47737263df6bd748
Nate <mac17039@byui.edu>
2019-10-01 10:38:28 -0600

    added stretch activity 1

--- web/team03/teamactivity.php
+++ web/team03/teamactivity.php
@@ -7,10 +7,20 @@
   <form action="form.php" method="post">
     Name: <input type="text" name="name"><br>
     E-mail: <input type="text" name="email"><br>
-    <input type="radio" name="major" value="Computer Science">Computer Science <br>
+
+    <?php 
+      $major = array("cs" => "Computer Science", "wdd" => "Web Design and Development", "cit" => "Computer Information Technology", "ce" => "Computer Engineering");
+
+    foreach($major as $x => $x_value) {
+      echo '<input type="radio" name="major" value="' . $x . '"> ' . $x_value . '<br/>';
+      }
+    ?>
+
+    <!-- <input type="radio" name="major" value="Computer Science">Computer Science <br>
     <input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
     <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
-    <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+    <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br> -->
+
     Comment: <textarea name="comment" rows="5" cols="40"></textarea><br>
 
     <input type="checkbox" name="continents[]" value="North America">North America<br>
