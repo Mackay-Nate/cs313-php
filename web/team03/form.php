cb70b161d9a89917fac68b3865e13ae15e4e4e75
Nate <mac17039@byui.edu>
2019-10-01 09:36:08 -0600

    cloning team activity

---
+++ web/team03/form.php
@@ -0,0 +1,21 @@
+<?php 
+  $continents = $_POST("continents");
+?>
+
+<html>
+<body>
+
+  Welcome <?php echo $_POST["name"]; ?><br>
+  Your email address is: <?php echo $_POST["email"]; ?><br>
+  Your major is: <?php echo $_POST["major"]; ?><br>
+  Your comments: <?php echo $_POST["comment"]; ?> <br>
+  Your continents are:
+  <?php
+    foreach ($continents as $value) { 
+      echo "$value <br>";
+    }
+  ?>
+
+
+</body>
+</html>
