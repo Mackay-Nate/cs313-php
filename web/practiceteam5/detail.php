802b128897872181b5ed757f7b17cfb983719960
Nate <mac17039@byui.edu>
2019-10-15 11:24:31 -0600

    added link and detail page

--- web/practiceteam5/detail.php
+++ web/practiceteam5/detail.php
@@ -24,38 +24,17 @@ catch (PDOException $ex)
 
 ?> 
 
-<h3>Scripture Resources</h3> 
+<h3>Scripture Details</h3> 
 
 <?php 
-  // core requirement 3
-  // foreach ($db->query('SELECT * FROM TestScriptures') as $row) { 
-  //   echo $row['book'] . ' ';
-  //   echo $row['chapter'] . ':';
-  //   echo $row['verse'] . ' - "';
-  //   // echo $row['content'] . '"'; 
-  //   echo '<br>';
-  }
-
-  //stretch 1
-  <form method="post" action=''>
-  	<span>Search:</span><input type="text" name="search">
-    <input type="submit" value="Search">
-  </form>
-	<?php
-    $book = filter_var($_POST["search"], FILTER_SANITIZE_STRING);
     foreach ($db->query("SELECT * FROM scriptures WHERE book='".$book."'") as $row) {
-      echo "<a href=\"detail.php\">";
       echo $row['book'] . ' ';
       echo $row['chapter'] . ':';
       echo $row['verse'] . ' - "';
-      echo "</a>";
+      echo $row['content'] . '.';
       echo '<br>';
     }
-	?>
-</table>
-
-
-
 ?>
+
 
 
