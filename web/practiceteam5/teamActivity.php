8f6b762bc9f8a6bb582f72f6feb603c5f61b5b23
Nate <mac17039@byui.edu>
2019-10-14 21:34:09 -0600

    initial team activity practice

---
+++ web/practiceteam5/teamActivity.php
@@ -0,0 +1,42 @@
+<?php 
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?> 
+
+<h3>Scripture Resources</h3> 
+
+<?php 
+  foreach ($db->query('SELECT * FROM TestScriptures') as $row) { 
+    echo $row['book'] . ' ';
+    echo $row['chapter'] . ':';
+    echo $row['verse'] . ' - "';
+    echo $row['contents'] . '"';
+    echo '<br>';
+  }
+
+
+
+?>
+
+
