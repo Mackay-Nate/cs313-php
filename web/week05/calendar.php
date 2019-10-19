d39d0fa21f1ba96d6490fe231dc4fdfd88126a2a
Nate <mac17039@byui.edu>
2019-10-19 01:18:09 -0600

    replace min/max with numbers

--- web/week05/calendar.php
+++ web/week05/calendar.php
@@ -59,16 +59,18 @@ Enter a random number to generate the meals. <br>
                JOIN Meal ON MenuItem.meal_id = Meal.id) 
                JOIN MealType ON MenuItem.meal_type = MealType.id)
                WHERE MealType.id = 1;";
-      $min =SELECT MIN(int(id)) 
-            FROM ((MenuItem
-            JOIN Meal ON MenuItem.meal_id = Meal.id) 
-            JOIN MealType ON MenuItem.meal_type = MealType.id)
-            WHERE MealType.id = 1;
-      $max =SELECT MAX(int(id)) 
-            FROM ((MenuItem
-            JOIN Meal ON MenuItem.meal_id = Meal.id) 
-            JOIN MealType ON MenuItem.meal_type = MealType.id)
-            WHERE MealType.id = 1;
+      $min = 1;
+      // SELECT MIN(int(id)) 
+      //       FROM ((MenuItem
+      //       JOIN Meal ON MenuItem.meal_id = Meal.id) 
+      //       JOIN MealType ON MenuItem.meal_type = MealType.id)
+      //       WHERE MealType.id = 1;
+      $max = 11;
+      // SELECT MAX(int(id)) 
+      //       FROM ((MenuItem
+      //       JOIN Meal ON MenuItem.meal_id = Meal.id) 
+      //       JOIN MealType ON MenuItem.meal_type = MealType.id)
+      //       WHERE MealType.id = 1;
             echo $min;
             echo $max; 
 
