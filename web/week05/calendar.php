5988b76cbfa9be64e9942b57d2fb2ae8409c7d5b
Nate <mac17039@byui.edu>
2019-10-19 03:14:19 -0600

    correcting dinner loop try 4

--- web/week05/calendar.php
+++ web/week05/calendar.php
@@ -174,7 +174,7 @@ Enter a random number to generate the meals. <br>
         }
       }
       echo $min;
-      echo $max
+      echo $max;
       $number = filter_var($_POST["random"], FILTER_SANITIZE_STRING);
       for ($i = $min + 10; $i < ($min + 5); $i++ ) { 
         $id = (($number + $i) % (($max - $min) + $min));
