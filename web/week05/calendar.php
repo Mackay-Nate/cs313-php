ef0b49cc6a0990d5655baa798c57c2b267c819c5
Nate <mac17039@byui.edu>
2019-10-19 00:42:58 -0600

    edit breakfast to add meals try 1

--- web/week05/calendar.php
+++ web/week05/calendar.php
@@ -58,17 +58,17 @@ Enter a random number to generate the meals. <br>
                FROM ((MenuItem
                JOIN Meal ON MenuItem.meal_id = Meal.id) 
                JOIN MealType ON MenuItem.meal_type = MealType.id)
-               WHERE MealType.id = 1;"
+               WHERE MealType.id = 1;";
       $min ="SELECT MIN(id) AS SmallestId 
              FROM ((MenuItem
              JOIN Meal ON MenuItem.meal_id = Meal.id) 
              JOIN MealType ON MenuItem.meal_type = MealType.id)
-             WHERE MealType.id = 1;"
+             WHERE MealType.id = 1;";
       $max ="SELECT MAX(id) AS LargestId 
              FROM ((MenuItem
              JOIN Meal ON MenuItem.meal_id = Meal.id) 
              JOIN MealType ON MenuItem.meal_type = MealType.id)
-             WHERE MealType.id = 1;"
+             WHERE MealType.id = 1;";
 
       $number = filter_var($_POST["random"], FILTER_SANITIZE_STRING);
       for ($i = $min; $i < $min + 5; $i++ ) { 
