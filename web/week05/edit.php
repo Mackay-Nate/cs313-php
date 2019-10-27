4ab2c914f0877d41a8d9b6b1a5146fa9f18f0917
Nate <mac17039@byui.edu>
2019-10-26 21:07:02 -0600

    add details to edit

--- web/week05/edit.php
+++ web/week05/edit.php
@@ -22,7 +22,7 @@
     echo 'Error!: ' . $ex->getMessage();
     die();
   }
-  
+
   $id = htmlspecialchars($_POST['search']);
   $prep = htmlspecialchars($_POST['']);
   $cook = htmlspecialchars($_POST['']);
@@ -52,10 +52,19 @@
     $mealName = filter_var($_POST["search"], FILTER_SANITIZE_STRING);
     foreach ($db->query("SELECT * FROM Meal WHERE name='$mealName' ") as $row) {
       $id = $row['id'];
+
       echo "<h3><a href='detail.php?id=$id'>";
       echo $row['name'];
       echo '</a></h3>';
+      echo '<br>'; 
+
+      echo  $row['name'] . ' takes ';
+      echo $row['prep'];
+      echo ' minutes to prepare it, and ' . $row['cook'] . ' minutes to cook it.';
       echo '<br>';
+      echo 'What would you like to change?'
+
+      
     }
 ?>
 
