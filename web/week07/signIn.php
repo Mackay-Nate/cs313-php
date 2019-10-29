fd01cbc0a65f6a7b093c3ce1df8eef1d8a38d0c4
Nate <mac17039@byui.edu>
2019-10-29 12:56:31 -0600

    week 7 team project

--- web/week07/signIn.php
+++ web/week07/signIn.php
@@ -23,68 +23,68 @@
   </nav>
 ​
   <!-- <?php
-    require 'password.php';
-​    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
-    {
-      try
-      {
-​
-        //connecting to database
-        $dbUrl = getenv('DATABASE_URL');
-        $dbOpts = parse_url($dbUrl);
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-​
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-​
-        //getting user info from database for comparision
-        $queryUser = 'SELECT userpassword FROM users WHERE username = :username';
-        $stmt = $db->prepare($queryUser);
-        //binding puts my variables into my place holders
-        $stmt->bindValue(':username', $_POST["username"], PDO::PARAM_STR);
-        $stmt->execute();
-​
-        //array that holds the results from the database
-        $userRowSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
-​
-        $hashedPassword = -1;
-        if(!empty($userRowSet))
-        {
-          $hashedPassword = $userRowSet[0]["userpassword"];
-​
-          if(password_verify($_POST["pwd1"], $hashedPassword))
-          {
-            $_SESSION["username"] = $_POST["username"];
-            header("Location: https://morning-bastion-33855.herokuapp.com/week7team/welcome.php");
-          }
-          else
-          {
-            header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signIn.php");
-          }
-        }
-        else
-        {
-        header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signIn.php");
-        }
-​
-​      }//end try
-      catch (PDOException $ex)
-      {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-      }
-      catch (Exception $ex)
-      {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-      }
-    }//end if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
-    else
-    {
+//     require 'password.php';
+// ​    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
+//     {
+//       try
+//       {
+// ​
+//         //connecting to database
+//         $dbUrl = getenv('DATABASE_URL');
+//         $dbOpts = parse_url($dbUrl);
+//         $dbHost = $dbOpts["host"];
+//         $dbPort = $dbOpts["port"];
+//         $dbUser = $dbOpts["user"];
+//         $dbPassword = $dbOpts["pass"];
+//         $dbName = ltrim($dbOpts["path"],'/');
+// ​
+//         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// ​
+//         //getting user info from database for comparision
+//         $queryUser = 'SELECT userpassword FROM users WHERE username = :username';
+//         $stmt = $db->prepare($queryUser);
+//         //binding puts my variables into my place holders
+//         $stmt->bindValue(':username', $_POST["username"], PDO::PARAM_STR);
+//         $stmt->execute();
+// ​
+//         //array that holds the results from the database
+//         $userRowSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// ​
+//         $hashedPassword = -1;
+//         if(!empty($userRowSet))
+//         {
+//           $hashedPassword = $userRowSet[0]["userpassword"];
+// ​
+//           if(password_verify($_POST["pwd1"], $hashedPassword))
+//           {
+//             $_SESSION["username"] = $_POST["username"];
+//             header("Location: https://morning-bastion-33855.herokuapp.com/week7team/welcome.php");
+//           }
+//           else
+//           {
+//             header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signIn.php");
+//           }
+//         }
+//         else
+//         {
+//         header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signIn.php");
+//         }
+// ​
+// ​      }//end try
+//       catch (PDOException $ex)
+//       {
+//         echo 'Error!: ' . $ex->getMessage();
+//         die();
+//       }
+//       catch (Exception $ex)
+//       {
+//         echo 'Error!: ' . $ex->getMessage();
+//         die();
+//       }
+//     }//end if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
+//     else
+//     {
     ?> -->
 ​
     <div>
